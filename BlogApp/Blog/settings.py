3a127efc0831a510b0ba02092cb878264fc92d49
DominikPyrek <pyrekdominik2025@gmail.com>
2025-04-16 00:28:43 +0200

    end for now

--- BlogApp/Blog/settings.py
+++ BlogApp/Blog/settings.py
@@ -139,3 +139,6 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ]
 }
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
--- BlogApp/blog/urls.py
+++ BlogApp/blog/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts/', views.posts),
-    path('category/', views.category),
-    path('comment/', views.comment),
-    path('register/', views.register),
-    path('login/', views.login)
+    path('/api/posts/', views.posts),
+    path('/api/category/', views.category),
+    path('/api/comment/', views.comment),
+    path('/api/register/', views.register),
+    path('/api/auth/', views.login)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
